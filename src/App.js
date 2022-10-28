81fbd00ca91f2c851abd3d4d0c8d2b3154e72e7e
David Habetyan <davidhabetyanm@gmail.com>
2022-10-28 16:45:40 +0400

    create Trello change and transfer text in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import TrelloTasks from "./components/trelloTask";
+import {useEffect, useState} from "react";
+import {tasksList} from './data/index'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [task, setTask] = useState([]);
+
+    useEffect(() => {
+        setTask(tasksList)
+    }, [])
+
+    const changeDescription = (id, newCategory) => {
+        setTask(task.map(text=> {
+            if(id===text.id){
+                text.description=  newCategory || text.description
+            }
+            return text;
+        }))
+    }
+
+const handleChangeTitle = (id, newTitle) => {
+   setTask(task.map(title => {
+      if(id === title.id) {
+         title.title = newTitle
+      }
+      return title
+   }))
+
+}
+
+
+    return (
+        <div className="App">
+            <TrelloTasks
+               handleChangeTitle={handleChangeTitle}
+               changeDescription={changeDescription}
+                name={"Todo"}
+                filter={task.filter(elem => elem.status === "todo")}
+            />
+            <TrelloTasks
+               handleChangeTitle={handleChangeTitle}
+               changeDescription={changeDescription}
+                name={"Done"}
+                filter={task.filter(elem => elem.status === "done")}
+            />
+
+            <TrelloTasks
+               handleChangeTitle={handleChangeTitle}
+               changeDescription={changeDescription}
+                name={"Blocked"}
+                filter={task.filter(elem => elem.status === "blocked")}
+            />
+
+            <TrelloTasks
+               handleChangeTitle={handleChangeTitle}
+               changeDescription={changeDescription}
+                name={"InProgress"}
+                filter={task.filter(elem=> elem.status === "inProgress")}
+            />
+        </div>
+    );
 }
 
 export default App;
